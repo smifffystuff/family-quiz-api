3566be198eb5606b842919579283e40acf5b332d
Martin Smith <martin.smith@dowjones.com>
2020-04-23 14:26:57 +0100

    Fix JWT Key

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -11,10 +11,7 @@ module.exports = (req, res, next) => {
       throw new Error('Authentication failed!');
     }
 
-    const decodedToken = jwt.verify(
-      token,
-      'my_super_secret_private_key_do_not_share'
-    );
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
     req.userData = {userId: decodedToken.userId};
     next();
   } catch (err) {
