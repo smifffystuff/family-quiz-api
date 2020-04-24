de32dc0c21a2c075cf93f9b22dd2e16a5dcc0066
Martin Smith <martin.smith@dowjones.com>
2020-04-24 09:44:31 +0100

    Add debug logging

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -11,16 +11,22 @@ const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
+      console.log('========= MULTER DESTINATION =================');
       cb(null, 'uploads/images');
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
+      console.log('========= MULTER FILENAME =================');
+      console.log(etx);
       cb(null, uuid() + '.' + ext);
     },
   }),
   fileFilter: (req, file, cb) => {
     const isValid = !!MIME_TYPE_MAP[file.mimetype];
     let error = isValid ? null : new Error('Invalid mime type');
+
+    console.log('========= MULTER FILEFILTER =================');
+    console.log(error);
     cb(error, isValid);
   },
 });
